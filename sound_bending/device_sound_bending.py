e708e9dd15d189fb53908a4f09d0b0ee12e87b42
Zack Wilkinson <44929504+zackjwilk@users.noreply.github.com>
2024-12-16 03:53:43 -0500

    Update device_sound_bending.py

--- sound_bending/device_sound_bending.py
+++ sound_bending/device_sound_bending.py
@@ -4,18 +4,34 @@ import midi
 import transport
 import mixer
 import general
+import time
 
 count = 0
+record_start = 0
 
-def toggle_record():
+def recording():
+    """
+    Returns True if currently recording.
+    Returns False if not.
+    """
+    global count
+    return count % 2 == 1
+
+def toggle_record(toggle, change_time=True):
     """
     Toggle audio recording.
     """
-    global count
+    global count, record_start
+
+    if toggle == count % 2:
+        return # if recording value from both scripts don't line up, don't toggle record
+
     transport.record()
     if count == 0:
-        transport.start()
-        count += 1
+        transport.start() # play track if this is first recording
+    count += 1
+    if change_time:
+        record_start = time.time()
     print("Toggled recording.")
 
 def undo():
@@ -38,12 +54,35 @@ def distortion():
     """
     mixer.enableTrack(4)
 
+def toggle_loop():
+    """
+    Set song position to beginning if end of loop
+    reached.
+    """
+    song_pos = transport.getSongPos(0)
+    song_len = transport.getSongLength(0)
+    if count > 1 and recording() and song_pos >= song_len: # if end of loop reached
+        toggle_record(0, False)
+        transport.setSongPos(0.0)
+        toggle_record(1, False) # wrap around to beginning of loop
+
+def record_cutoff():
+    """
+    Stop recording if max length reached.
+    """
+    global count, record_start
+
+    record_len = time.time() - record_start
+    song_len = transport.getSongLength(0)/1000 # convert ms to s
+    if count > 1 and recording() and record_len >= song_len:
+        toggle_record(0)
+
 def OnMidiMsg(event):
     event.handled = False
     if event.midiId == midi.MIDI_NOTEOFF:
         channel = event.status & 0x0F
         if channel == 1:
-            toggle_record()
+            toggle_record(event.note) # note of 0 = stop record, 1 = start record
         elif channel == 2:
             undo()
         elif channel == 3:
@@ -52,3 +91,6 @@ def OnMidiMsg(event):
             update_reverb_delay(reverb, delay)
         elif channel == 4:
             distortion()
+        elif channel == 5:
+            toggle_loop()
+            record_cutoff()
